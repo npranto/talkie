5191add5036aec9dfab65cc2d545c45e1d8e1328
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-05-01 01:58:27 -0400

    sets up socket connections between the server and the client

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client'
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: []
+    }
+  }
+
+  componentDidMount() {
+    const socket = io();
+    console.log('Running socket on the client...')
+  }
+
   render() {
     return (
       <div className="App">
@@ -9,6 +24,7 @@ class App extends Component {
       </div>
     );
   }
+
 }
 
 export default App;
